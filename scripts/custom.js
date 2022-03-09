2d4937d2b27ba1cf7bc1e68d6b6ae201d7041020
John Henry <55134289@ad.mmu.ac.uk>
2022-03-09 11:37:51 +0000

    Removed PHP as not compatible. Replaced functionality with JS and jQuery

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){    
     $("#profile").fadeIn("slow");   
+    $("#top-bar").load("site/nav.html")
+
+    if($("#blog-text").length){
+        // We are on a blog page, get the appropriate blog.
+        getBlog();
+    }
 });
+
+function getBlog(){    
+    let searchURL = new URLSearchParams(window.location.search)
+    let blogURL = "";
+    if(searchURL.has("name"))
+    {
+        if(searchURL.get("name") != "")
+        {
+            blogURL = "blogs/" + searchURL.get("name") + ".html";
+            $("#blog-text").load(blogURL);
+        }
+        else
+        {
+            window.location.href = "/personal-blog-jh";
+        }
+    }
+    else
+    {
+        window.location.href = "/personal-blog-jh";
+    }  
+}
 
 $.fn.isInViewport = function() {
     var elementTop = $(this).offset().top;
@@ -29,15 +56,5 @@ $(window).on('resize scroll', function() {
     else
     {
         $("#h-posts").hide();
-    }
-    // if($("#main").isInViewport())
-    // {
-    //     $("#main").show();
-    // }
-    // else
-    // {
-    //     $("#main").hde();
-    // }
-
-     
+    }     
 });
